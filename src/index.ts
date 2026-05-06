9d2bc61ca67ad92fd18606eface09c80ff40b0fb
molang404 <art104233@naver.com>
2026-05-06 14:24:40 +0900

    Prisma 준비 작업 완료

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 
-// 환경설정 파일을 불러오는 dotenv 라이브러리 호출
+
 dotenv.config();
 
-// 백엔드를 구성하는 express 앱 만들기
 const app = express();
 
-// 환경 변수 총 key가 PORT인 값을 가져오되, 가져올 수 없다면 8080을 PORT 변수에 할당
-// 환경 변수에서 가져오는 값은 string | undefined
 const PORT = process.env.PORT || "8080";
 
-// 실제 app 구동
-// app.listen(예비포트번호, 서버가 실행되면서 해야하는 함수)
-app.listen(8000, () => {
+// express 앱에 기능을 확장할 때에는 app.use() 메서드를 사용
+
+// express.json() : 요청(Request)의 본문(body)에서 JSON 데이터를 객체로 변환(파싱)하여 request.body에 저장
+app.use(express.json());
+
+// express.urlencoded() : 요청(Request)의 본문(body)에서 URL-encoded 데이터를 객체로 변환(파싱)하여 request.body에 저장
+// URL은 한글을 원래 포함할 수 없기 때문에 변환을 하게 되는데, 그것을 한글로 받아들일 수 있도록 하는 기능
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(PORT, () => {
     console.log(`서버 실행됨! http://localhost:${PORT}`);
 });
