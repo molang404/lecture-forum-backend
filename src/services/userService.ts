ed17f144202f5c1118ce647f4010a9192325dc63
molang404 <art104233@naver.com>
2026-05-08 12:35:13 +0900

    에러 수정

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,7 +4,7 @@ import { Prisma } from "../generated/prisma/client.ts";
 
 const createUser = async (data: UserCreateInput) => {
     try {
-    return prisma.user.create({
+    return await prisma.user.create({
         data,
     });
     } catch (error) {
@@ -14,22 +14,22 @@ const createUser = async (data: UserCreateInput) => {
             if (error.code === "P2002") {
                 // 중복된 칼럼이 어떤 것인지에 대한 정보는
                 // error.meta?.target에 들어있는데 이 프로퍼티 타입은 string[] | undefined
-                const target = error.meta?.target as string[];
+                const errorMessage = error.message;
 
                 // 예시. target = {"username", "nickname"}
                 // array의 요소 중 "이 값"이 있는지 확인하는 메서드는 .includes()
                 // .find()와 비슷한 역할이지만,
                 // find는 조건을 걸어서 찾을 수 있는 메서드이고 (리턴값은 찾은 그 요소)
                 // includes는 단순히 집어넣은 값과 완벽히 일치하는 것이 있는지 true/false로 찾음
-                if (target?.includes("username")) {
+                if (errorMessage.includes("username")) {
                     // 상위함수로 새로운 자바스크립트의 표준 객체를 만들어서 던짐
                     // 그 내용은 "ALREADY_EXISTS_USERNAME"이라고 담음
                     throw new Error("ALREADY_EXISTS_USERNAME");
                     }
-                if (target?.includes("email")) {
+                if (errorMessage.includes("email")) {
                     throw new Error("ALREADY_EXISTS_EMAIL");
                 }
-                if (target?.includes("nickname")) {
+                if (errorMessage.includes("nickname")) {
                     throw new Error("ALREADY_EXISTS_NICKNAME");
                 }
                 throw error;      // return과 같은데 값을 리턴하는게 아니라 에러를 리턴하는 키워드
