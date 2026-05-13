34b7cdb721ca0fc4e7844dc7ddd4dd1090cc4fe7
molang404 <art104233@naver.com>
2026-05-13 17:12:46 +0900

    초기화

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,1 @@
-import dotenv from "dotenv";
-import express from "express";
-import userRouter from "./routes/userRouter.ts";
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-// 프론트앤드가 하는 요청(Request)에 대하여 경로
-app.use("/user", userRouter);
-
-app.listen(PORT, () => {
-    console.log(`Server listening on : http://localhost:${PORT}`);
-});
