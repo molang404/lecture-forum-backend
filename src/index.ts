281f7d1915ac617ef801f65bd7ddc0f4484dbf80
molang404 <art104233@naver.com>
2026-05-14 12:48:10 +0900

    cors 적용

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
 import userRouter from "./routes/userRouter.ts";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// 교차 출처 리소스 공유 (CORS)를 허용하는 건 백엔드에서 증명하여 허용해야 함
+// cors() 만 사용하면 모든 프론트앤드 주소에 대해 허용 증명을 하는 것
+// cors({ origin : "주소" }) 를 통해 특정 주소에 대해서만 허용을 증명할 수 있음
+app.use(cors());
 
 app.use(express.json());
 
