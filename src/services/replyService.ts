013912d37a7accb839e9e67336a4586cbe8b175c
molang404 <art104233@naver.com>
2026-06-04 12:37:41 +0900

    replyController.deleteReply 작성 완료

--- src/services/replyService.ts
+++ src/services/replyService.ts
@@ -68,7 +68,27 @@ const createReply = async (userId: number, postId: number, content: string) => {
     // 이렇게 prisma.reply.create()를 실행하면, 생성"된" Reply 객체가 리턴
 };
 
+export const deleteReply = async (id: number, userId: number) => {
+    const reply = await prisma.reply.findUnique({
+        where: {
+            id,
+        },
+    });
+    if (!reply) {
+        throw new Error("NOT_FOUND_REPLY");
+    }
+    if (reply.userId === userId) {
+        throw new Error("FORBIDDEN");
+    }
+    prisma.reply.delete({
+        where: {
+            id,
+        },
+    });
+};
+
 export default {
     createReply,
     getRepliesByPostId,
+    deleteReply,
 };
