c4b310fe5c8cc35c7fee906da2209719d7cd9e03
molang404 <art104233@naver.com>
2026-06-15 15:17:14 +0900

    경로 수정

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -14,11 +14,11 @@ router.post("/create", validate(createUserSchema), userController.createUser);
 router.post("/login", validate(loginSchema), userController.login);
 router.patch("/update", authenticate, validate(updateUserSchema), userController.updateUser);
 router.patch("/password", authenticate, validate(updatePasswordSchema), userController.updatePassword);
-router.delete("/withdraw", authenticate, validate(withdrawUserSchema), userController.withdrawUser);
+router.patch("/withdraw", authenticate, validate(withdrawUserSchema), userController.withdrawUser);
 
 // 회원 탈퇴 => 실제 데이터베이스에서 그 데이터를 삭제하지 않을 것임
 //              소프트 삭제를 통해 deletedAt에 탈퇴 시간을 기록하는 방식으로 처리
-//              path, delete 둘다 말이 됨
+//              delete는 body를 전달해야 돼서 불가능
 
 // 똑같은 경로라도 여러개의 라우터 가능(경로, 방식 둘다 보기 때문)
 
